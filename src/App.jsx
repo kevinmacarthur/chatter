b88c82ae4084cc67eb9019eeafe707765db1c7be
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-29 23:33:43 +0000

    inital commit, made basic components for start of chatter app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
+import ChatBar from './ChatBar.jsx'
+import Message from './Message.jsx'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <NavBar />
+        <Message />
+        <ChatBar />
+      </div>
+
     );
   }
 }
+
+class NavBar extends Component {
+  render() {
+    return (
+     <nav className="navbar">
+      <a href="/" className="navbar-brand">Chatty</a>
+    </nav>
+    );
+  }
+}
+
 export default App;
+
---
+++ src/Message.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Message extends Component {
+  render() {
+    return (
+      <main className="messages">
+        <div className="message">
+          <span className="message-username">Anonymous1</span>
+          <span className="message-content"> I won't be impressed with technology until I can download food.</span>
+        </div>
+      </main>
+    );
+  }
+}
+
+// Should this be a seperate component
+class MessageSystem extends Component {
+  render() {
+    return (
+      <div className="message system">
+        Anonymous1 changed their name to nomnom.
+      </div>
+    )
+  }
+}
+export default Message;
