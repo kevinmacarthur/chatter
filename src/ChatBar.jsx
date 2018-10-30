13d34ef5fedc4c5500b8167159603527cc54bf65
kevinmacarthur <kmacarthur16@gmail.com>
2018-10-30 17:30:19 +0000

    added messages to update with a new entered message from the chatbar

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+  constructor(props) {
+      super(props);
+      this.keyPress = this.keyPress.bind(this);
+   }
+
+   handleChange(e) {
+      this.setState({ value: e.target.value });
+   }
+
+   keyPress(e){
+      if(e.keyCode == 13){
+        this.props.addMessage(e.target.value);
+         e.target.value = ''
+      }
+   }
   render() {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" placeholder={this.props.currentUser} />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" onKeyDown={this.keyPress} placeholder="Type a message and hit ENTER" />
       </footer>
     )
   }
